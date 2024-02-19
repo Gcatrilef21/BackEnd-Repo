181f88bd32ca6522fc86f4c02bd26606ea2ead37
Gonzalo Catrilef <catrilef.gonzalo@icloud.com>
2024-02-18 22:07:09 -0300

    Actualizacion CSS-Router-APP-Funcionando

--- 04-Websocket/src/app.js
+++ 04-Websocket/src/app.js
@@ -1,18 +1,27 @@
 import express from 'express';
-const PORT = 3001;
+import { engine } from 'express-handlebars';
+import __dirname from './utils.js'
+import {join} from 'path'
+import { router as routerViews } from './routes/vistasRouter.js';
+
+const PORT = 8082;
 
 const app = express();
 
+let rutaV = join(__dirname,'views')
+let rutaP = join(__dirname, 'public')
+
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(rutaP))
 
-app.get('/', (req,res)=>{
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', rutaV)
+
+app.use('/', routerViews )
 
 
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).json({Message:'FUUUUAP, Acuestate'});
-})
-
-const serverHTTP=app.listen(PORT,()=>{
+const server = app.listen(PORT, () => {
     console.log(`Server escuchando en puerto ${PORT}`);
 });
---
+++ 04-Websocket/src/routes/vistasRouter.js
@@ -0,0 +1,10 @@
+import { Router } from 'express' ;
+
+export const router = Router()
+
+router.get('/', (req,res)=>{
+
+
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).render('index',{ title : 'MMMCheese'});
+})
